5af3bc88342a386885a763394570ddf48822b076
Slantext <53126986+Slantext@users.noreply.github.com>
2020-03-12 11:59:07 -0700

    Add files via upload

---
+++ PyFinance06.py
@@ -0,0 +1,32 @@
+# import datetime as dt
+# import matplotlib.pyplot as plt
+# import mpl_finance
+# from matplotlib import style
+# from mpl_finance import candlestick_ohlc
+# import matplotlib.dates as mdates
+# import pandas as pd
+# import pandas_datareader.data as web
+# from pandas.plotting import register_matplotlib_converters
+# register_matplotlib_converters()
+
+import bs4 as bs
+import pickle
+import requests
+
+def save_sp500_tickers():
+	resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+	soup = bs.BeautifulSoup(resp.text, 'lxml')
+	table = soup.find('table', {'class':'wikitable sortable'})
+	tickers = []
+	for row in table.findAll('tr')[1:]:
+		ticker = row.findAll('td')[0].text
+		tickers.append(ticker)
+
+	with open("sp500tickers.pickle", "wb") as f:
+		pickle.dump(tickers, f)
+
+		print(tickers)
+
+	return tickers 
+
+save_sp500_tickers()
